4668bde3355b2e4c04e3e11c79a47b7a596bc235
Lucas Gueiros <lucasdantasgueiros@gmail.com>
2016-05-03 21:02:44 -0300

    Colocando estrutura básica de Professor.

--- novoprojeto/src/java/br/edu/ifpe/garanhuns/provapc/negocio/Professor.java
+++ novoprojeto/src/java/br/edu/ifpe/garanhuns/provapc/negocio/Professor.java
@@ -24,4 +24,60 @@ public class Professor {
     private long id;
     @Column
     private int siap;
+
+    public Professor() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getSiap() {
+        return siap;
+    }
+
+    public void setSiap(int siap) {
+        this.siap = siap;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + (int) (this.id ^ (this.id >>> 32));
+        hash = 29 * hash + this.siap;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Professor other = (Professor) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (this.siap != other.siap) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Professor{" + "id=" + id + ", siap=" + siap + '}';
+    }
+    
+    
+    
 }
