098d4a0786d759492400adfcee7c63c6f7b9ec2a
Luc Melkkas <Luc Melkkas>
2016-05-03 21:59:13 -0300

    Repositorio alternativa correção

--- novoprojeto/src/java/br/edu/ifpe/garanhuns/provapc/persistencia/dao/RepositorioAlternativa.java
+++ novoprojeto/src/java/br/edu/ifpe/garanhuns/provapc/persistencia/dao/RepositorioAlternativa.java
@@ -5,10 +5,30 @@
  */
 package br.edu.ifpe.garanhuns.provapc.persistencia.dao;
 
+import br.edu.ifpe.garanhuns.provapc.negocio.Alternativa;
+import br.edu.ifpe.garanhuns.provapc.negocio.Prova;
+import java.util.List;
+
 /**
  *
  * @author LUCINALDO
  */
 public class RepositorioAlternativa {
+    private final DaoManagerHiber dao = DaoManagerHiber.getInstance();
     
+    public void adicionar(Alternativa a) {
+        dao.persist(a);
+    }
+    public void remover(Alternativa a) {
+        dao.delete(a);
+    }
+    public void  alterar(Alternativa a) {
+        dao.update(a);
+    }
+    public Alternativa recupearar(long id){
+        return (Alternativa) dao.recover("from Alternativa where id="+id).get(0);
+    }
+    public List<Alternativa> recuperarTodos(){
+        return DaoManagerHiber.getInstance().recover("from Alternativa");
+    }
 }
