0c7bbe4a13603e266704bfc53904fc4cac1c554c
Lucas Gueiros <lucasdantasgueiros@gmail.com>
2016-05-24 21:29:07 -0300

    Concertando exite() e recuperar no Repositório Prova.

--- novoprojeto/src/java/br/edu/ifpe/garanhuns/provapc/persistencia/repositorios/RepositorioProva.java
+++ novoprojeto/src/java/br/edu/ifpe/garanhuns/provapc/persistencia/repositorios/RepositorioProva.java
@@ -31,11 +31,10 @@ public class RepositorioProva {
         dao.update(p);
     }
     public boolean existe(long id) {
-        Prova p = (Prova) dao.recover("from Prova where id = :param", id).get(0);
-        return p!=null;
+        return ! dao.recover("from Prova where id = :id", id).isEmpty();
     }
     public Prova recupearar(long id){
-        return (Prova) dao.recover("from Prova where id=:param",id).get(0);
+        return (Prova) dao.recover("from Prova where id=:id",id).get(0);
     }
       public List<Prova> recuperarTodos(){
         return DaoManagerHiber.getInstance().recover("from Prova");
