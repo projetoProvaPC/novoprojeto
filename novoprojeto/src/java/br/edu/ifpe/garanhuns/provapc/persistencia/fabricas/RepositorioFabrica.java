975171d7acd368fa9ed7482f7bc0245073133595
Lucas Gueiros <lucasdantasgueiros@gmail.com>
2016-06-11 18:48:59 -0300

    mend

--- novoprojeto/src/java/br/edu/ifpe/garanhuns/provapc/persistencia/fabricas/RepositorioFabrica.java
+++ novoprojeto/src/java/br/edu/ifpe/garanhuns/provapc/persistencia/fabricas/RepositorioFabrica.java
@@ -10,19 +10,34 @@ import br.edu.ifpe.garanhuns.provapc.persistencia.repositorios.interfaces.Reposi
 import br.edu.ifpe.garanhuns.provapc.persistencia.repositorios.interfaces.RepositorioQuestao;
 
 /**
- *
+ * Use para conseguir repositórios. Caso queira alterar de Memoria para BD ou outra coisa, mude dentro do método getInstance.
  * @author lucas
  */
 public abstract class RepositorioFabrica {
     
     private static RepositorioFabrica fabrica = null;
     protected RepositorioFabrica(){}
+    /**
+     * Isso vai lhe dar uma Fábrica de Repositórios.
+     * @return uma fábrica de repositórios.
+     */
     public static RepositorioFabrica getInstance() {
         if(fabrica == null) fabrica = new RepositorioFabricaMemoria();
         return fabrica;
     }
-    
+    /**
+     * Retorna um repositório para provas
+     * @return repositório de provas
+     */
     public abstract RepositorioProva getRepositorioProva();
+    /**
+     * Retorna um repositório para alternativas
+     * @return repositório de alternativas
+     */
     public abstract RepositorioAlternativa getRepositorioAlternativa();
+    /**
+     * Retorna um repositório para questões
+     * @return repositório para questões.
+     */
     public abstract RepositorioQuestao getRepositorioQuestao();
 }
