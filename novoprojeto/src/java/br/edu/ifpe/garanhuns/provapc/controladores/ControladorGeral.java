334be40ee019176a888ef15506fe9bb92f7b0a5a
thaismelo <thaiscapoeiras1999@hotmail.com>
2016-06-11 18:27:35 -0300

    removendo botões de cadastrar e colocando o de finalizar prova

--- novoprojeto/src/java/br/edu/ifpe/garanhuns/provapc/controladores/ControladorGeral.java
+++ novoprojeto/src/java/br/edu/ifpe/garanhuns/provapc/controladores/ControladorGeral.java
@@ -7,7 +7,9 @@ package br.edu.ifpe.garanhuns.provapc.controladores;
 
 import br.edu.ifpe.garanhuns.provapc.construtores.ProvaBuilder;
 import br.edu.ifpe.garanhuns.provapc.construtores.QuestaoBuilder;
+import br.edu.ifpe.garanhuns.provapc.negocio.Prova;
 import br.edu.ifpe.garanhuns.provapc.negocio.Questao;
+import br.edu.ifpe.garanhuns.provapc.persistencia.repositorios.implementacoes.RepositorioProvaBD;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
 
@@ -20,9 +22,19 @@ import javax.faces.bean.ManagedBean;
 public class ControladorGeral {
     
     private ProvaBuilder prova = new ProvaBuilder();
+    private RepositorioProvaBD repositorio = new RepositorioProvaBD();
     
     public List<QuestaoBuilder> getQuestoes() {
         return prova.getQuestoes();
     }
     
+    
+     public String finalizarProva(Prova p) {
+        if(repositorio.existe(p.getId())) {
+            repositorio.atualizar(p);
+        } else {
+            repositorio.adicionar(p);
+        }
+         return "ApresentarProva.xhtml";
+    }
 }
